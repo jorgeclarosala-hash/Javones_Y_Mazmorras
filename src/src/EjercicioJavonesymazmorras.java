32451150c5f10b757013da00b0931d23895bb112
jorgeclarosala-hash <jorgeclarosala@alumnes.ilerna.com>
2026-02-08 21:33:49 +0100

    El constructor "EjercicioJavonesymazmorras" es super importante porque define y manipula los valores de los objetos

--- src/src/EjercicioJavonesymazmorras.java
+++ src/src/EjercicioJavonesymazmorras.java
@@ -1,5 +1,14 @@
 package src;
 
 public class EjercicioJavonesymazmorras {
+//trabajo, esta va a ser la clase base donde estaran los contructores, sera la clase padre de todos los personajes del juego.
+protected String nombre;//Estos son los objetos que transferiran informacion que pueden ir cambiando de 
+protected int nivel;
+protected int puntosVida;
+protected boolean protegido;
+protected Inventario inventario;
 
+public EjercicioJavonesymazmorras(String nombre, int nivel, int puntosVida){
+
+}
 }
